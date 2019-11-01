0e932a306b91f3618ee7bc7238c50f91ec124de8
ivoelbert <ivo.elbert1@gmail.com>
2019-11-01 19:07:18 -0300

    Reestructure public/

--- src/printables/controller.ts
+++ src/printables/controller.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express-serve-static-core';
-import { promises as promiseFs } from 'fs';
+import { promises as promiseFs, Dirent } from 'fs';
+
+type PrintableData = {
+    printables: string[];
+};
 
 export const listModels = async (req: Request, res: Response) => {
-    const files: string[] = await promiseFs.readdir('public');
+    const files: Dirent[] = await promiseFs.readdir('public', { withFileTypes: true });
 
-    const listData = {
-        printables: files.filter((fileName: string) => fileName.endsWith('.gltf')),
+    const listData: PrintableData = {
+        printables: files.filter((dirent: Dirent) => dirent.isDirectory()).map((dirent: Dirent) => `${dirent.name}.gltf`),
     };
 
     res.status(200).send(listData);
