7ee818f47160e2f8d5fffe0f3803765250c2da7e
ivoelbert <ivo.elbert1@gmail.com>
2019-11-02 18:46:27 -0300

    Send jpg thumbnail. TODO: settle on conventions

--- src/printables/controller.ts
+++ src/printables/controller.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express-serve-static-core';
 import { promises as promiseFs, Dirent } from 'fs';
 
+type Printable = {
+    modelSrc: string;
+    thumbSrc: string;
+};
 type PrintableData = {
-    printables: string[];
+    printables: Printable[];
 };
 
 export const listModels = async (req: Request, res: Response) => {
     const files: Dirent[] = await promiseFs.readdir('public', { withFileTypes: true });
 
     const listData: PrintableData = {
-        printables: files.filter((dirent: Dirent) => dirent.isDirectory()).map((dirent: Dirent) => `${dirent.name}/${dirent.name}.gltf`),
+        printables: files
+            .filter((dirent: Dirent) => dirent.isDirectory())
+            .map((dirent: Dirent) => ({ modelSrc: `${dirent.name}/${dirent.name}.gltf`, thumbSrc: `${dirent.name}/${dirent.name}.jpg` })),
     };
 
     res.status(200).send(listData);
